ede551a35cda46a3441ffb8386bce9a92c3e4ae3
Pavittar Singh <singh.pavittar95@icloud.com>
2020-07-31 15:46:57 +0530

    list and page component added

--- packages/neo-theme/src/components/index.js
+++ packages/neo-theme/src/components/index.js
@@ -1,19 +1,29 @@
 import React from "react";
 import { Global, css, connect, styled, Head } from "frontity";
-import Link from './link';
-const Root = ({ state }) => {
-  console.log("state----------", state);
+import Switch from "@frontity/components/switch";
+import Link from "./link";
+import List from "./list";
+import Page from "./page";
 
+const Root = ({ state }) => {
+  const data = state.source.get(state.router.link);
+  console.log("data----------", data);
   return (
     <>
       You can edit your package in:
       <pre>packages/neo-theme/src/index.js</pre>
-      <p> Current url:  {state.router.link}</p>
+      <p> Current url: {state.router.link}</p>
       <nav>
-      <Link href="/">Home</Link>
-      <Link href="/page/2">More Posts</Link>
-      <Link href="/lorem-ipsum">Lorem ipsum</Link>
+        <Link href="/">Home</Link>
+        <Link href="/page/2">More Posts</Link>
+        <Link href="/lorem-ipsum">Lorem ipsum</Link>
       </nav>
+      <main>
+        <Switch>
+          <List when={data.isArchive} />
+          <Page when={data.isPostType} />
+        </Switch>
+      </main>
     </>
   );
 };
